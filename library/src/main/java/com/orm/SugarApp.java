ae15e49fe29dae1be6c15e3ad3aba1df8d17d386
Francesco Lombardi <francesco.lombardi09@gmail.com>
2016-09-19 18:01:50 +0200

    Integrazione sqlCipher in sugar lib

--- library/src/main/java/com/orm/SugarApp.java
+++ library/src/main/java/com/orm/SugarApp.java
@@ -2,11 +2,15 @@ package com.orm;
 
 import android.app.Application;
 
+import net.sqlcipher.database.SQLiteDatabase;
+
+
 public class SugarApp extends Application {
 
     @Override
     public void onCreate() {
         super.onCreate();
+        InitializeSQLCipher();
         SugarContext.init(this);
     }
 
@@ -14,6 +18,16 @@ public class SugarApp extends Application {
     public void onTerminate() {
         super.onTerminate();
         SugarContext.terminate();
+    }
+
+    private void InitializeSQLCipher() {
+        SQLiteDatabase.loadLibs(this);
+/*        File databaseFile = getDatabasePath("demo.db");
+        databaseFile.mkdirs();
+        databaseFile.delete();
+        SQLiteDatabase database = SQLiteDatabase.openOrCreateDatabase(databaseFile, "test123", null);
+        database.execSQL("create table t1(a, b)");
+        database.execSQL("insert into t1(a, b) values(?, ?)", new Object[]{"one for the money", "two for the show"});*/
     }
 
 }
